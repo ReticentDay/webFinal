3c3d6cd61b042fbae3035ff57ebca694130fb6ec
ReticentDay <aa532199@gmail.com>
2017-06-16 22:29:51 +0800

    add js

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 mixins = require('postcss-mixins');
+webpack = require('webpack');
 
 gulp.task('default',function(){
   console.log('Hello GULP');
@@ -37,12 +38,25 @@ gulp.task('controlStyles',function(){
   .pipe(gulp.dest('./app/styles'));
 });
 
+gulp.task('scripts',function(callback){
+  wabpack(require('./webpack.config.js') , function(err, stats){
+    if(err){
+      console.log(err.toString());
+    }
+
+    console.log(stats.toString());
+    callback();
+  });
+});
+
 gulp.task('watch',function(){
 
-  watch('./app/css/*.css',function(){
+  watch('./app/css/**/*.css',function(){
     gulp.start('styles');
     gulp.start('indexStyles');
     gulp.start('controlStyles');
   });
-
+  watch('.app/scripts/*.js',function(){
+    gulp.start('scripts');
+  });
 });
